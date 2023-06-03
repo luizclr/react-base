d843b073d2ff2d147b96449a58f995665bdc5d08
Luiz Claudio <luiz.lclr@gmail.com>
2023-06-02 21:20:16 -0300

    chore(packages): update packages' version

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ const app = express();
 const root = join(__dirname, "dist");
 app.use(express.static(root));
 app.use(fallback("index.html", { root }));
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 4000);
